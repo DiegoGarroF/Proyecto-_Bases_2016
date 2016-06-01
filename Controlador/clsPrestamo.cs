20a84a1ab8450757ddd67da73fc4c1af59f5bd0a
Pánfilo Herrera <luis0270@gmail.com>
2016-06-01 16:48:28 -0600

    Se agrega la entidad de prestamo

---
+++ Controlador/clsPrestamo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Modelo;
+
+namespace Controlador
+{
+    public class clsPrestamo
+    {
+        private string strSentencia;
+
+        public Boolean mInsertarCliente(clsConexionSQL conexion,  clsEntidadPrestamo pEntidadPrestamo)
+        {
+            strSentencia = "insert into prestamo(fechaInicio,fechaFin, nombreEstudiante, carnet) values('" + pEntidadPrestamo.setGetFechaInicio +"')";
+            return false;
+        }
+
+
+    }
+}
---
+++ Modelo/clsEntidadPrestamo.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    public class clsEntidadPrestamo
+    {
+        #region Atributos
+        private string fechaInicio;
+        private string fechaFin;
+        private string nombreEstudiante;
+        private string carnet;
+        #endregion
+
+        #region Set y get
+
+        public string setGetFechaInicio 
+        {
+            get { return fechaInicio; }
+            set { fechaInicio = value; }
+        }
+
+        public string setGetFechaFin
+        {
+            get { return fechaFin; }
+            set { fechaFin = value; }
+        }
+
+        public string setGetNombreEstudiante
+        {
+            get { return nombreEstudiante; }
+            set { nombreEstudiante = value; }
+        }
+
+        public string setGetCarnet
+        {
+            get { return carnet; }
+            set { carnet = value; }
+        }
+
+
+        #endregion
+
+    }
+}

5b88b795a5a99fcf6e9bc82c1d05f431c9f781de
Pánfilo Herrera <luis0270@gmail.com>
2016-06-06 17:02:34 -0600

    Se agrega entidad usuario

---
+++ Controlador/clsUsuario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Modelo;
+
+namespace Controlador
+{
+    public class clsUsuario
+    {
+        private string sentencia = "";
+
+
+        public void mBuscarUsuario(clsConexion conexion,clsEntidadUsuario pEntidadUsuario)
+        {
+            sentencia = "select idUsuario, usuario, contrasena from tbUsuario where idUsuario=?";
+        }
+    }
+}

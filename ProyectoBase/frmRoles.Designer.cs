aeb0823504c6e85893a5671b956b500b85be07b1
AlejandraAO <alearroyoo@gmail.com>
2016-06-15 10:14:54 -0600

    Cambios roles

---
+++ ProyectoBase/frmRoles.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace ProyectoBase
+{
+    partial class frmRoles
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.chkInsertar = new System.Windows.Forms.CheckBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.chkConsultar = new System.Windows.Forms.CheckBox();
+            this.chkEliminar = new System.Windows.Forms.CheckBox();
+            this.chkModificar = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cbPantalla = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lbNombre = new System.Windows.Forms.Label();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.chkInsertar);
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.chkConsultar);
+            this.groupBox2.Controls.Add(this.chkEliminar);
+            this.groupBox2.Controls.Add(this.chkModificar);
+            this.groupBox2.Location = new System.Drawing.Point(411, 37);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(307, 200);
+            this.groupBox2.TabIndex = 29;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Privilegios";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(214, 171);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 33;
+            this.button1.Text = "Eliminar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 13);
+            this.label3.TabIndex = 26;
+            this.label3.Text = "Privilegios de pantalla:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(117, 171);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 32;
+            this.button2.Text = "Modificar";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // chkInsertar
+            // 
+            this.chkInsertar.AutoSize = true;
+            this.chkInsertar.Enabled = false;
+            this.chkInsertar.Location = new System.Drawing.Point(21, 61);
+            this.chkInsertar.Name = "chkInsertar";
+            this.chkInsertar.Size = new System.Drawing.Size(61, 17);
+            this.chkInsertar.TabIndex = 27;
+            this.chkInsertar.Text = "Insertar";
+            this.chkInsertar.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(36, 171);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 31;
+            this.button3.Text = "Agregar";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // chkConsultar
+            // 
+            this.chkConsultar.AutoSize = true;
+            this.chkConsultar.Enabled = false;
+            this.chkConsultar.Location = new System.Drawing.Point(21, 84);
+            this.chkConsultar.Name = "chkConsultar";
+            this.chkConsultar.Size = new System.Drawing.Size(70, 17);
+            this.chkConsultar.TabIndex = 28;
+            this.chkConsultar.Text = "Consultar";
+            this.chkConsultar.UseVisualStyleBackColor = true;
+            // 
+            // chkEliminar
+            // 
+            this.chkEliminar.AutoSize = true;
+            this.chkEliminar.Enabled = false;
+            this.chkEliminar.Location = new System.Drawing.Point(21, 127);
+            this.chkEliminar.Name = "chkEliminar";
+            this.chkEliminar.Size = new System.Drawing.Size(62, 17);
+            this.chkEliminar.TabIndex = 30;
+            this.chkEliminar.Text = "Eliminar";
+            this.chkEliminar.UseVisualStyleBackColor = true;
+            // 
+            // chkModificar
+            // 
+            this.chkModificar.AutoSize = true;
+            this.chkModificar.Enabled = false;
+            this.chkModificar.Location = new System.Drawing.Point(21, 108);
+            this.chkModificar.Name = "chkModificar";
+            this.chkModificar.Size = new System.Drawing.Size(69, 17);
+            this.chkModificar.TabIndex = 29;
+            this.chkModificar.Text = "Modificar";
+            this.chkModificar.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cbPantalla);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.btnEliminar);
+            this.groupBox1.Controls.Add(this.btnModificar);
+            this.groupBox1.Controls.Add(this.btnAgregar);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.lbNombre);
+            this.groupBox1.Location = new System.Drawing.Point(12, 37);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(353, 200);
+            this.groupBox1.TabIndex = 28;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Roles";
+            // 
+            // cbPantalla
+            // 
+            this.cbPantalla.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbPantalla.Enabled = false;
+            this.cbPantalla.FormattingEnabled = true;
+            this.cbPantalla.Location = new System.Drawing.Point(142, 67);
+            this.cbPantalla.Name = "cbPantalla";
+            this.cbPantalla.Size = new System.Drawing.Size(101, 21);
+            this.cbPantalla.TabIndex = 27;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(74, 67);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 13);
+            this.label4.TabIndex = 26;
+            this.label4.Text = "Pantalla:";
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(228, 154);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 25;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Location = new System.Drawing.Point(131, 154);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(75, 23);
+            this.btnModificar.TabIndex = 24;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(50, 154);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 23;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(142, 24);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(154, 20);
+            this.textBox1.TabIndex = 22;
+            // 
+            // lbNombre
+            // 
+            this.lbNombre.AutoSize = true;
+            this.lbNombre.Location = new System.Drawing.Point(74, 27);
+            this.lbNombre.Name = "lbNombre";
+            this.lbNombre.Size = new System.Drawing.Size(63, 13);
+            this.lbNombre.TabIndex = 21;
+            this.lbNombre.Text = "Nombre Rol";
+            // 
+            // frmRoles
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(726, 300);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "frmRoles";
+            this.Text = "frmRoles";
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox chkInsertar;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.CheckBox chkConsultar;
+        private System.Windows.Forms.CheckBox chkEliminar;
+        private System.Windows.Forms.CheckBox chkModificar;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox cbPantalla;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lbNombre;
+    }
+}

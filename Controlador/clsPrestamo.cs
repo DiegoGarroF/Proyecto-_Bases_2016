9b5be6da5c44e3b0d037a20355b4c65aa12236d8
Erick Gonzalez <Erick Gonzalez@ERICK>
2016-06-28 00:03:43 -0600

    Realizando Cambios en los permisos de Prestamos

--- Controlador/clsPrestamo.cs
+++ Controlador/clsPrestamo.cs
@@ -43,6 +43,22 @@ namespace Controlador
             strSentencia = "delete from tbPestamo where idLibro=@idLibro and idUsuarioCliente=@idUsuarioCliente";
             return conexion.mEjecutarElimModif(strSentencia, conexion, pEntidadPrestamo, tipo);
         }
-        
+
+        //Optener los roles del usuario conectado sobre la ventana de Libro
+        public SqlDataReader mObtenerRolesUsuario(clsConexion conexion, String codigo, String contrasena)
+        {
+            strSentencia = "SELECT modificar,insertar,consultar,eliminar  from tbRolPantalla RP , tbRol R  ,tbPantalla P ,tbUsuarioRol  UR  , tbUsuario U  where U.idUsuario=UR.idUsuario and P.idPantalla=RP.idPantalla and R.idRol=UR.idRol and R.idRol=RP.idRol and U.idUsuario=@codigo and P.nombre=@contrasena";
+            return conexion.mSeleccionarLogueo(strSentencia, codigo, contrasena);
+        }
+        public SqlDataReader mObtenerPrivilegiosDirectos(clsConexion conexion, String codigo, String contrasena)
+        {
+            strSentencia = "SELECT modificar,insertar,consultar,eliminar  from tbUsuarioPantalla UP , tbPantalla P where UP.idUsuario=@codigo and UP.idPantalla=P.idPantalla and P.nombre=@contrasena";
+            return conexion.mSeleccionarLogueo(strSentencia, codigo, contrasena);
+        }
+        public SqlDataReader mSeleccionarIdUsuario(clsConexion conexion, String usuario)
+        {
+            strSentencia = "SELECT idUsuario from tbUsuario where usuario=@codigo";
+            return conexion.mSeleccionarTipoString(strSentencia, usuario);
+        }
     }
 }
